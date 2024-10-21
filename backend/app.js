908bcb9ab1cf9bfb8d4bc17669998bb9e272ac1b
AmanBhadkariya <140033975+AmanBhadkariya@users.noreply.github.com>
2024-10-21 22:11:40 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 const server = createServer(app);
