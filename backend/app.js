88fce6dd96202e15b399a04acf129f9b1250f26d
AmanBhadkariya <140033975+AmanBhadkariya@users.noreply.github.com>
2024-10-23 14:36:50 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5174",
+    origin: "https://one0wpm.onrender.com",
     methods: ['GET', 'POST'],
     credentials: true,
   }
