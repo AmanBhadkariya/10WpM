2b967d13be27a3de43ea88464eb59e90c4ffe694
AmanBhadkariya <140033975+AmanBhadkariya@users.noreply.github.com>
2026-07-04 12:20:24 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,15 @@ import socket from './socket/socket'
 function App() {
   const [inRoom, setInRoom] = useState(false)
   const [raceActive, setRaceActive] = useState(false)
-  const floatingKeys = ['A', 'TYPE', 'S', 'WPM', 'D', 'FLOW', 'F', 'RACE', 'J', 'FAST', 'K', '⌘', 'L', '↵', 'GO!', '10WPM']
+  const floatingKeys = ['A', 'TYPE', 'S', 'WPM', 'D', 'FLOW', 'F', 'RACE', 'J', 'FAST', 'K', '⌘', 'L', '↵', 'GO!', '10WPM',
+      '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
+
+      'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\',
+
+      'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'",
+
+      'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/'
+  ]
 
   useEffect(() => {
     const start = () => setRaceActive(true)
