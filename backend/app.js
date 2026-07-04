55fb72130981778ccc344d50ac9a367c669015ff
AmanBhadkariya <140033975+AmanBhadkariya@users.noreply.github.com>
2026-07-04 11:52:31 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-  cors: { origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://one0wpm.onrender.com"], methods: ["GET", "POST"], credentials: true },
+  cors: { origin: ["https://one0wpm.onrender.com","http://localhost:5173", "http://127.0.0.1:5173"], methods: ["GET", "POST"], credentials: true },
 });
 
 const wordPools = {
